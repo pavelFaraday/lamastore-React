3cd98e4447f99a2c580d2687ed2c744e2e36f00b
pavelFaraday <devstudio.corp@gmail.com>
2022-12-27 20:49:13 +0400

    filter products

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,30 +1,50 @@
 import React, { useState } from "react";
 import List from "../../Components/List/List";
 import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch.js";
 import "./Products.scss";
 
 const Products = () => {
 	const catId = parseInt(useParams().id);
 	const [maxPrice, setMaxPrice] = useState(1000);
 	const [sort, setSort] = useState(null);
+	const [selectedSubCats, setSelectedSubCats] = useState([]);
+
+	const { data, loading, error } = useFetch(
+		`/sub-categories?[filters][categories][id][$eq]=${catId}`
+	);
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		const isChecked = e.target.checked;
+
+		setSelectedSubCats(
+			isChecked
+				? [...selectedSubCats, value]
+				: selectedSubCats.filter((item) => item !== value)
+		);
+
+		console.log(selectedSubCats);
+	};
 
 	return (
 		<div className="products">
 			<div className="left">
 				<div className="filterItem">
 					<h2>Product Categories</h2>
-					<div className="inputItem">
-						<input type="checkbox" id="1" value={1} />
-						<label htmlFor="1">Shoes</label>
-					</div>
-					<div className="inputItem">
-						<input type="checkbox" id="2" value={2} />
-						<label htmlFor="2">Skirts</label>
-					</div>
-					<div className="inputItem">
-						<input type="checkbox" id="3" value={3} />
-						<label htmlFor="3">Coats</label>
-					</div>
+					{data?.map((item) => (
+						<div className="inputItem" key={item.id}>
+							<input
+								type="checkbox"
+								id={item.id}
+								value={item.id}
+								onChange={handleChange}
+							/>
+							<label htmlFor={item.id}>
+								{item.attributes.title}
+							</label>
+						</div>
+					))}
 				</div>
 				<div className="filterItem">
 					<h2>Filter By Price</h2>
@@ -69,7 +89,12 @@ const Products = () => {
 					className="catImg"
 					alt=""
 				/>
-				<List catId={catId} maxPrice={maxPrice} sort={sort} />
+				<List
+					catId={catId}
+					maxPrice={maxPrice}
+					sort={sort}
+					subCats={selectedSubCats}
+				/>
 			</div>
 		</div>
 	);
