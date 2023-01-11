3b5a6c2cd140113d5e3857c94f6cc134e7769375
pavelFaraday <devstudio.corp@gmail.com>
2023-01-11 18:39:21 +0400

    finish filtering

--- client/src/Components/List/List.js
+++ client/src/Components/List/List.js
@@ -7,7 +7,7 @@ const List = ({ subCats, maxPrice, sort, catId }) => {
 	const { data, loading, error } = useFetch(
 		`/products?populate=*&[filters][categories][id][$eq]=${catId}${subCats.map(
 			(item) => `&[filters][sub_categories][id][$eq]=${item}`
-		)}`
+		)}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
 	);
 	return (
 		<div className="list">
